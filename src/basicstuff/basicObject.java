8112f71e979396eb81cf67d9b6ff8b90c6922760
christopherrehm <christopherrehm@squeaker>
2017-06-29 00:47:53 +0200

    whole project Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ src/basicstuff/basicObject.java
@@ -0,0 +1,51 @@
+package basicstuff;
+
+/*
+ * Copyright (C) 2017 christopherrehm.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+/**
+ *
+ * @author christopherrehm
+ */
+public class basicObject {
+    // this is the basic object that all objects are derived from it has the following properties
+    // creates and implements analysis
+    // creates and implements basic message sending anywhere in the system
+    // maintains a list of who is accessing this object lock problems 
+    // write data to txt file 
+    // read data from txt file
+    private long timerTotal = 0 ;
+    private long startTime = 0 ;
+    private long timerStop = 0 ; 
+
+    public basicObject() {
+       startTime = this.getCurrentTime();
+       
+    }
+    public long calcluateTimeDiff(long startTime, long endTime) {
+        return endTime-startTime;
+        
+    }
+    
+    public long getCurrentTime() {
+        return System.currentTimeMillis();
+    }
+            
+    
+}
