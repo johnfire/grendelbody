1c03f5b3376a617bf0fae84ecbe3adcd13bc4d1c
christopherrehm <christopherrehm@squeaker>
2017-06-29 19:59:20 +0200

    some updates more Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelbody/internetinout/internetInterface.java
+++ src/grendelbody/internetinout/internetInterface.java
@@ -6,16 +6,28 @@
 package grendelbody.internetinout;
 
 import basicstuff.basicObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author christopherrehm
  */
 public class internetInterface extends basicObject {
-    
+    int runVar = 1;
     @Override
     public void run() {
-        System.out.println("we are in the internet interface routine");
+        System.out.println("we are in the internet interface routine first time"
+                + "");
+        while (runVar== 1){
+            try {
+                Thread.sleep(5000);
+                System.out.println("we are in the internet interface routine");  
+            } catch (InterruptedException ex) {
+                Logger.getLogger(internetInterface.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        }
     }
     
 }
