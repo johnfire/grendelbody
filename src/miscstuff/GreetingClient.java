026dee53c9bbd50150fc8bcca3c6e249665891cb
christopherrehm <christopherrehm@squeaker>
2017-07-01 21:01:36 +0200

    spacing fix Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ src/miscstuff/GreetingClient.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 christopherrehm.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package miscstuff;
+
+/**
+ *
+ * @author christopherrehm
+ */
+
+// File Name GreetingClient.java
+import java.net.*;
+import java.io.*;
+
+public class GreetingClient {
+
+   public static void main(String [] args) {
+      String serverName = args[0];
+      int port = Integer.parseInt(args[1]);
+      try {
+         System.out.println("Connecting to " + serverName + " on port " + port);
+         Socket client = new Socket(serverName, port);
+         
+         System.out.println("Just connected to " + client.getRemoteSocketAddress());
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream out = new DataOutputStream(outToServer);
+         
+         out.writeUTF("Hello from " + client.getLocalSocketAddress());
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream in = new DataInputStream(inFromServer);
+         
+         System.out.println("Server says " + in.readUTF());
+         client.close();
+      }catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
