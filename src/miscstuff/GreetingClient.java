91c7cf8a8dc736d913cf5a2ab61ff3d486783a8a
christopherrehm <christopherrehm@squeaker>
2017-07-02 18:05:47 +0200

    server update for objects Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/miscstuff/GreetingClient.java
+++ src/miscstuff/GreetingClient.java
@@ -29,103 +29,63 @@ import java.awt.event.ActionListener;
 import java.net.*;
 import java.io.*;
 import java.awt.event.ActionEvent;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 public class GreetingClient extends basicstuff.basicObject{
     private BufferedReader in;
     private PrintWriter out;
+     private Socket clientSocket;
 
-
+    public GreetingClient(String string, int i) { //To change body of generated methods, choose Tools | Templates.
+    }
+   
     /**
      * Constructs the client by laying out the GUI and registering a
      * listener with the textfield so that pressing Enter in the
      * listener sends the textfield contents to the server.
+     * @throws java.io.IOException
      */
-    public GreetingClient(String computer, int port) throws IOException {
-             Socket mySocket = new Socket(computer, port );
-
+ 
+    public void startConnection(String ip, int port) {
         try {
-            BufferedReader msgIn = null;
             try {
-                msgIn = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-                PrintWriter msgOut = new PrintWriter(mySocket.getOutputStream(), true);
+                clientSocket = new Socket(ip, port);
+                System.out.println("in GreetingClient, establised socket");
             } catch (IOException ex) {
                 Logger.getLogger(GreetingClient.class.getName()).log(Level.SEVERE, null, ex);
-            } finally {
-                try {
-                    msgIn.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(GreetingClient.class.getName()).log(Level.SEVERE, null, ex);
-                }
             }
-            
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                if (".".equals(inputLine)) {
-                    out.println("good bye");
-                    break;
-                }
-                out.println(inputLine);
-            }
-            
-            
-            /**
-             * Runs the client application.
-             */
-//    public static void main(String[] args) throws Exception {
-//        CapitalizeClient client = new CapitalizeClient();
-//        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        client.frame.pack();
-//        client.frame.setVisible(true);
-//        client.connectToServer();
-//    }
-//   public void run(String aServer, int port) {
-//      String serverName = aServer;
-//      try {
-//         System.out.println("Connecting to " + serverName + " on port " + port);
-//         Socket client = new Socket(serverName, port);
-//         
-//         System.out.println("Just connected to " + client.getRemoteSocketAddress());
-//         
-//         OutputStream outToServer = client.getOutputStream();
-//         DataOutputStream out = new DataOutputStream(outToServer);
-//         System.out.println("past data stream setup");
-//         
-//         outToServer.("Hello from " + client.getLocalSocketAddress());
-//         
-//         InputStream inFromServer = client.getInputStream();
-//         DataInputStream in = new DataInputStream(inFromServer);
-//         
-//         System.out.println("Server says " + in.readUTF());
-//         //client.close();
-//      }catch(IOException e) {
-//         e.printStackTrace();
-//      }
-//   }
-//   
-//
-//        
-//    }
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(GreetingClient.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+   
+    public String sendMessage(String msg) {
+        out.println(msg);
+        
+        String resp ="";
+        try {
+            resp = in.readLine();
+            System.out.println(resp);
         } catch (IOException ex) {
             Logger.getLogger(GreetingClient.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return resp;
     }
-     public void sendMessage(message amessage){
-        try{
-            System.out.println("entered sendmessage method");
-            Socket client = new Socket("192.168.0.101", 5000);
-            System.out.println("111111entered sendmessage method");
-            // Create the input & output streams to the server
-            ObjectOutputStream outToServer = new ObjectOutputStream(client.getOutputStream());
-            //ObjectInputStream inFromServer = new ObjectInputStream(client.getInputStream());
-            System.out.println("22222entered sendmessage method");
-            outToServer.writeObject(amessage);
-            System.out.print("just sent a message");
-                
-        }catch(IOException e){
-            System.out.println("something failed in GreetingClient.sendMessage");
-        }
-     }
+    
+    public void sendMessageObject (message myMessage) throws IOException{
+        ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+        outToServer.writeObject(myMessage); 
+    }
+    
+    public message ReceiveMessageObject () throws IOException, ClassNotFoundException{
+        message newMessage = new message();
+        ObjectInputStream fromServer = new ObjectInputStream(clientSocket.getInputStream());
+        newMessage = (message) fromServer.readObject();
+        System.out.println(newMessage);
+        return newMessage;
+    }
 }
