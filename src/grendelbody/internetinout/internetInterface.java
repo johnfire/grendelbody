6d50f789b105dc74c75132c57675650ed22f92e2
christopherrehm <christopherrehm@squeaker>
2017-07-02 18:06:16 +0200

    server update for objects Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelbody/internetinout/internetInterface.java
+++ src/grendelbody/internetinout/internetInterface.java
@@ -22,34 +22,38 @@ public class internetInterface extends basicObject implements Runnable {
     int pid;
     @Override
     public void run() {
+        
         message TestMessage =new message();
+        
+        // get process id number
         try {
              pid = Integer.parseInt(new File("/proc/self").getCanonicalFile().getName());
         } catch (IOException ex) {
             Logger.getLogger(internetInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
         GreetingClient myClient = null;
+        myClient = new GreetingClient("192.168.0.101",5000);
+        
+        myClient.startConnection("192.168.0.101",5000);
+        System.out.println("-----made contact from internetinterface to router");
+        
         try {
-            myClient = new GreetingClient("192.168.0.101",5000);
+            myClient.sendMessageObject(TestMessage);
         } catch (IOException ex) {
             Logger.getLogger(internetInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        //myClient.start("192.168.0.101",5000);
-        System.out.println("made contact from internetinterface to router");
-        myClient.sendMessage(TestMessage);
-        System.out.println("just past send message statement");
-                
-        System.out.println("we are in the internet interface routine first time"+ "my process ID is "+ pid);
-        while (runVar== 1){
-            try {
-                Thread.sleep(5000);
-                System.out.println("we are in the internet interface routine, process number "+pid);
-                Thread.getAllStackTraces().keySet().forEach((t) -> System.out.println(t.getName() + " Is Daemon " + t.isDaemon() + " Is Alive " + t.isAlive()));
-
-            } catch (InterruptedException ex) {
-                Logger.getLogger(internetInterface.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        System.out.println("-----just past send message statement");
+               
+//        while (runVar== 1){
+//            try {
+//                System.out.println("we are in the internet interface routine, process number "+pid);
+//                Thread.getAllStackTraces().keySet().forEach((t) -> System.out.println(t.getName() + " Is Daemon " + t.isDaemon() + " Is Alive " + t.isAlive()));
+//                Thread.sleep(5000);
+//            } catch (InterruptedException ex) {
+//                Logger.getLogger(internetInterface.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
     }   
 }
