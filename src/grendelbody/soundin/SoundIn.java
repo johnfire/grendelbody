67f0d03d293ad921092682fc8427e76553f619bf
christopherrehm <christopherrehm@squeaker>
2017-07-06 15:30:13 +0200

    update for message passing to and from all body parts Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelbody/soundin/SoundIn.java
+++ src/grendelbody/soundin/SoundIn.java
@@ -5,7 +5,14 @@
  */
 package grendelbody.soundin;
 
+import basicstuff.Message;
 import basicstuff.ObjectStatus;
+import grendelbody.internetinout.InternetInterface;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import miscstuff.GreetingClient;
 
 /**
  *
@@ -14,16 +21,58 @@ import basicstuff.ObjectStatus;
 public class SoundIn extends basicstuff.BasicObject {
     
     int MyId = 4;
+    public LinkedList<Message> myMessagesToSend;
+    public LinkedList<Message> myMessagesToRead;
+    int port = 5000;
+    String iPAddress ="192.198.0.101";
+    
+    public SoundIn() {
+        myMessagesToSend = new LinkedList(); 
+        myMessagesToRead = new LinkedList();
+    } 
     
     @Override
     public void run() {
         
-        System.out.println("we are in the soundin routine");
+        this.systemMessageStartUp("starting soundin run routine");
+        
         
         ObjectStatus myStats = new basicstuff.ObjectStatus();
         myStats.setMyName("soundin cell");
         Thread soundInThread = new Thread(myStats);
         soundInThread.start(); 
-        this.systemMessageStartUp("started sound in thread");
+        this.systemMessageStartUp("started Sound In self monitoring thread");
+
+        
+        //this.startObjStatus("Sound In");
+        
+        // set up connection 
+        GreetingClient myClient = null;
+        myClient = new GreetingClient("192.168.0.101",5000);
+        myClient.startConnection("192.168.0.101",5000);
+        
+        
+        this.systemMessage("-----SoundIn Cell-----made contact from Sound Cell to router");
+        // enter work loop 
+        while(true){
+            try {
+                // send all mesasges in list
+            while(this.myMessagesToSend.isEmpty() != true){
+                myClient.sendMessageObject(this.myMessagesToSend.removeFirst());
+                this.systemMessage("-----SoundIn Cell----- sent a message");
+            }
+            while(this.myMessagesToRead.isEmpty() != true){
+                try {
+                    this.myMessagesToRead.addLast(myClient.receiveMessageObject());
+                    this.systemMessage("-----SoundIn Cell----- just received a message");
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(InternetInterface.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            } catch (IOException ex) {
+                Logger.getLogger(InternetInterface.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //this.systemMessage("-----InternetInterface-----leaving work loop statement");
+        }
     }    
 }
