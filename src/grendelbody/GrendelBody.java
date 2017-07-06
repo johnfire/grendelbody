433d0ed642858c813980f998941f733fdc1f9c12
christopherrehm <christopherrehm@squeaker>
2017-07-06 14:31:15 +0200

    now passing two messages to the server.... Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- src/grendelbody/GrendelBody.java
+++ src/grendelbody/GrendelBody.java
@@ -20,23 +20,27 @@ import grendelbody.visionin.VisionIn;
  */
 public class GrendelBody extends BasicObject {
     public static final int  myId = 11; 
-
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        int delayTime =1000; // needed for socket sconnections
         // TODO code application logic here
         //
         try {
             InternetInterface myInternetInterface = new InternetInterface();
             Thread theInternetInterface = new Thread(myInternetInterface);
             Output myOutput = new Output();
+            Thread.sleep(delayTime);
             Thread theOutputer = new Thread(myOutput);
             SoundIn mySoundIn = new SoundIn();
+            Thread.sleep(delayTime);
             Thread theListener = new Thread(mySoundIn);
             VisionIn myVisionIn= new VisionIn();
+            Thread.sleep(delayTime);
             Thread theEyes = new Thread(myVisionIn);
+            Thread.sleep(delayTime);
             ThreadTracker myThreadTracker = new ThreadTracker();
             Thread theTracker = new Thread(myThreadTracker);
             
